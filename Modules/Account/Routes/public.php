a17402fd0818b3c98474d9dc0e832a9639a5c29d
verplancken <42614785+verplancken@users.noreply.github.com>
2021-02-23 11:37:23 -0600

    Initial commit

---
+++ Modules/Account/Routes/public.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function () {
+    Route::get('account', 'AccountDashboardController@index')->name('account.dashboard.index');
+
+    Route::get('account/profile', 'AccountProfileController@edit')->name('account.profile.edit');
+    Route::put('account/profile', 'AccountProfileController@update')->name('account.profile.update');
+
+    Route::get('account/orders', 'AccountOrderController@index')->name('account.orders.index');
+    Route::get('account/orders/{id}', 'AccountOrderController@show')->name('account.orders.show');
+
+    Route::get('account/wishlist', 'AccountWishlistController@index')->name('account.wishlist.index');
+
+    Route::get('account/reviews', 'AccountReviewController@index')->name('account.reviews.index');
+
+    Route::get('addresses', 'AccountAddressController@index')->name('account.addresses.index');
+    Route::post('addresses', 'AccountAddressController@store')->name('account.addresses.store');
+    Route::put('addresses/{id}', 'AccountAddressController@update')->name('account.addresses.update');
+    Route::delete('addresses/{id}', 'AccountAddressController@destroy')->name('account.addresses.destroy');
+
+    Route::post('addresses/change-default-address', 'AccountDefaultAddressController@update')->name('account.change_default_address');
+});
